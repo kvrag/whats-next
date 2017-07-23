df03817f174624c692fa0eff1397bf053c543956
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-23 13:39:29 -0500

    Spins controller

---
+++ app/controllers/spins_controller.rb
@@ -0,0 +1,13 @@
+class SpinsController < ApplicationController
+
+  def create
+    @user = User.find(params[:user_id])
+    redirect_to user_spin_path(@user)
+  end
+
+  def show 
+    @user = User.find(params[:user_id])
+    @feeling = Feeling.find_by(name: @user.top_feeling)
+    render 'spins#show'
+  end
+end
